e2e1e58eb1393386a394e05a622627ecec97729a
NAKUL CHAUHAN <nakul.11jics067@jietjodhpur.ac.in>
2018-06-04 10:43:30 +0530

    Update CheckHttpdConfig.sh

--- scripts/CheckHttpdConfig.sh
+++ scripts/CheckHttpdConfig.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 sudo echo "<VirtualHost *:80>
-    ServerName nakul.com
-    ServerAlias www.nakul.com
-    DocumentRoot /var/www/html/nakul-website
-    ErrorLog ${APACHE_LOG_DIR}/nakul-website_error.log
-    CustomLog ${APACHE_LOG_DIR}/nakul-website_access.log combined
+    ServerName subodh.com
+    ServerAlias www.subodh.com
+    DocumentRoot /var/www/html/Subodh-website
+    ErrorLog ${APACHE_LOG_DIR}/Subodh-website_error.log
+    CustomLog ${APACHE_LOG_DIR}/Subodh-website_access.log combined
 </VirtualHost>" >> /etc/httpd/conf/httpd.conf
 
 status=$(service httpd configtest 2>&1 | grep "OK" | wc -l)
